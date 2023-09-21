a04c2dc10d66eb0726d36c0ca333c9f852fd6677
yoavlv <yoavlv12@gmail.com>
2023-09-21 10:46:16 +0300

    test

--- Tests/TestUtils.py
+++ Tests/TestUtils.py
@@ -1,4 +1,8 @@
-
+import sys
+import os
+current_directory = os.path.dirname(os.path.abspath(__file__))
+root_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+sys.path.append(root_directory)
 import unittest
 import pandas as pd
 from unittest.mock import patch, Mock
