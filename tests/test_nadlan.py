69cea2aa89111f51ef62af00d8ffd2bbb347ad64
yoavlv <yoavlv12@gmail.com>
2024-02-06 17:13:35 +0200

    update

--- tests/test_nadlan.py
+++ tests/test_nadlan.py
@@ -7,13 +7,13 @@ import numpy as np
 
 @pytest.fixture
 def input_data():
-    try:
-        from ..dev import get_db_engine
-        engine = get_db_engine(db_name='nadlan_db',)
-        query = "SELECT * FROM nadlan_raw LIMIT 100"
-        df = pd.read_sql_query(query, engine)
-    except:
-        df = pd.read_csv('nadlan_data.csv',index_col=0)
+    # try:
+    #     from ..dev import get_db_engine
+    #     engine = get_db_engine(db_name='nadlan_db',)
+    #     query = "SELECT * FROM nadlan_raw LIMIT 100"
+    #     df = pd.read_sql_query(query, engine)
+    # except:
+    df = pd.read_csv('nadlan_data.csv',index_col=0)
     return df
 
 def test_pre_process(input_data):
